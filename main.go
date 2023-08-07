668445e161a43c499cc91f38a3774b55f76fbaef
waynewu411 <waynewu411@gmail.com>
2023-08-07 15:54:52 +1000

    Use a fan-in error channel to indicate if the goroutines have stopped and if there is any error

--- main.go
+++ main.go
@@ -4,135 +4,136 @@ import (
 	"fmt"
 	"github.com/waynewu411/go-producer-consumer-demo/config"
 	"log"
-	"sync"
 	"time"
 )
 
 func main() {
-	_ = config.InitConfig()
+	cfg := config.InitConfig()
 
-	// Channels for communication between producer and consumers
-	errorChan := make(chan error)   // Send errors from goroutines to main goroutine
-	doneChan := make(chan struct{}) // Signal when all goroutines are done
+	err := producerConsumer(cfg.ConsumerCount)
+	log.Printf("ProducerConsumer finished, error: %v", err)
+}
 
-	// WaitGroup to wait for all goroutines to finish
-	var wg sync.WaitGroup
+func producerConsumer(consumerCount int) error {
+	errChans := make([]chan error, 0, 1+consumerCount) // Send errors from goroutines to main goroutine
 
 	// Start the producer goroutine
 	producerDataChan := make(chan int)
-	stopProducer := make(chan struct{}) // Stop signal for the producer
-	wg.Add(1)
-	go producer(producerDataChan, errorChan, stopProducer, doneChan, &wg)
+	producerErrChan := make(chan error)
+	errChans = append(errChans, producerErrChan)
+	stopProducerChan := make(chan struct{}) // Stop signal for the producer
+	go producer(producerDataChan, producerErrChan, stopProducerChan)
 
 	// Start the consumer goroutines
-	consumerDataChans := make([]chan int, config.GetConfig().ConsumerCount)
-	stopWorkers := make([]chan struct{}, config.GetConfig().ConsumerCount) // Slice of stop signals for the consumers
-	for i := 0; i < config.GetConfig().ConsumerCount; i++ {
+	consumerDataChans := make([]chan int, consumerCount)
+	stopConsumers := make([]chan struct{}, consumerCount) // Slice of stop signals for the consumers
+	for i := 0; i < consumerCount; i++ {
 		consumerDataChans[i] = make(chan int)
-		stopWorkers[i] = make(chan struct{})
-		wg.Add(1)
-		go consumer(i, consumerDataChans[i], errorChan, stopWorkers[i], doneChan, &wg)
+		consumerErrChan := make(chan error)
+		errChans = append(errChans, consumerErrChan)
+		stopConsumers[i] = make(chan struct{})
+		go consumer(i, consumerDataChans[i], consumerErrChan, stopConsumers[i])
 	}
 
 	// start a goroutine for distributing data from producer to consumers
 	go FanOutWithChannel(producerDataChan, consumerDataChans...)
 
-	// Monitor for errors from goroutines and done signal
-	select {
-	case err := <-errorChan:
-		log.Printf("Main goroutine: Received error: %v\n", err)
+	mergedErrChan := FanIn(errChans...)
 
-		// Send the stop signal to all goroutines
-		close(stopProducer)
-		for i := 0; i < config.GetConfig().ConsumerCount; i++ {
-			close(stopWorkers[i])
+	var mergedError error = nil
+	for i := 0; i < 1+consumerCount; i++ {
+		err := <-mergedErrChan
+		log.Printf("[main]: Received error: %v", err)
+		if err != nil {
+			mergedError = err
+			close(stopProducerChan)
+			for i := 0; i < consumerCount; i++ {
+				close(stopConsumers[i])
+			}
 		}
-
-		// Wait for all goroutines to finish
-		wg.Wait()
-		log.Println("All goroutines stopped.")
-
-	case <-doneChan:
-		log.Println("All goroutines completed without errors.")
 	}
+	return mergedError
 }
 
-func producer(dataChan chan<- int, errorChan chan<- error, stopSignal <-chan struct{}, doneChan chan<- struct{}, wg *sync.WaitGroup) {
+func producer(dataChan chan<- int, errChan chan<- error, stopSignal <-chan struct{}) {
+	var err error = nil
+
 	defer func() {
-		if r := recover(); r != nil {
-			errorChan <- fmt.Errorf("%v", r)
+		r := recover()
+		if r != nil {
+			errChan <- fmt.Errorf("%v", r)
+		} else {
+			errChan <- err
 		}
 	}()
-
-	defer wg.Done()
 
 	// Simulate reading data from an external source
 	for i := 1; i <= 100; i++ {
 		// Check for the stop signal
 		select {
 		case <-stopSignal:
-			log.Println("producer: Received stop signal. Stopping.")
+			log.Println("[producer] received stop signal. Stopping.")
 			return
 		default:
 		}
 
 		if config.GetConfig().ProducerFailNumber >= 0 && i == config.GetConfig().ProducerFailNumber {
-			errorChan <- fmt.Errorf("producer: Simulated error while reading data %v", i)
+			err = fmt.Errorf("[producer] fail on %v", i)
 			return
 		}
 
 		if config.GetConfig().ProducerPanicNumber >= 0 && i == config.GetConfig().ProducerPanicNumber {
-			panic(fmt.Sprintf("producer panic on %d", i))
+			panic(fmt.Sprintf("[producer] panic on %d", i))
 		}
 
 		// Send data to the consumers
 		dataChan <- i
-		log.Printf("producer sent %d", i)
+		log.Printf("[producer] sent %d", i)
 		time.Sleep(time.Duration(config.GetConfig().ProducerSendInternal) * time.Millisecond)
 	}
 
 	// Close the data channel when done
 	close(dataChan)
-
-	// Signal that the producer is done
-	doneChan <- struct{}{}
 }
 
-func consumer(id int, dataChan <-chan int, errorChan chan<- error, stopSignal <-chan struct{}, doneChan chan<- struct{}, wg *sync.WaitGroup) {
+func consumer(id int, dataChan <-chan int, errChan chan<- error, stopSignal <-chan struct{}) {
+	var err error = nil
+
 	defer func() {
-		if r := recover(); r != nil {
-			errorChan <- fmt.Errorf("%v", r)
+		r := recover()
+		if r != nil {
+			errChan <- fmt.Errorf("%v", r)
+		} else {
+			errChan <- err
 		}
 	}()
-
-	defer wg.Done()
 
 	for {
 		select {
 		case data, ok := <-dataChan:
 			if !ok {
 				// Data channel has been closed
-				log.Printf("[consumer %d] Data channel closed. Stopping.\n", id)
+				log.Printf("[consumer %d] data channel closed. Stopping.", id)
 				return
 			}
 
 			// Process the data
 			if config.GetConfig().ConsumerFailNumber >= 0 && data == config.GetConfig().ConsumerFailNumber {
 				// Suppose an error occurs in one of the consumers
-				errorChan <- fmt.Errorf("[consumer %d] fail to process data %v", id, data)
+				err = fmt.Errorf("[consumer %d] fail on %v", id, data)
 				return
 			}
 
 			if config.GetConfig().ConsumerPanicNumber >= 0 && data == config.GetConfig().ConsumerPanicNumber {
-				panic(fmt.Sprintf("[consumer %d] panic on processing data %d", id, data))
+				panic(fmt.Sprintf("[consumer %d] panic on %d", id, data))
 			}
 
-			log.Printf("[consumer %d] processed data %d\n", id, data)
+			log.Printf("[consumer %d] processed %d", id, data)
 			time.Sleep(time.Duration(config.GetConfig().ConsumerReadInternal) * time.Millisecond)
 
 		case <-stopSignal:
 			// Received stop signal from the main goroutine
-			log.Printf("[consumer %d] Received stop signal. Stopping.\n", id)
+			log.Printf("[consumer %d] received stop signal. Stopping.", id)
 			return
 		}
 	}
